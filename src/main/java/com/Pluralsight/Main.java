d96d701d2eae3a9c56bcbfc47531674704ed90f0
kadrianafuller <kadrianafuller@icloud.com>
2025-01-17 11:21:03 -0600

    Updating screen manager class

--- src/main/java/com/Pluralsight/Main.java
+++ src/main/java/com/Pluralsight/Main.java
@@ -1,20 +1,8 @@
 package com.Pluralsight;
- // ALL THESE ARE IMPORTED SO I CAN USE
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Scanner; // imported so I can use my scanner
+
 
 public class Main {
-<<<<<<< HEAD
-=======
-   // public static ArrayList<Transactions> transactions;
-    //Comment by Tumelo to test commit & push on new branch
-    //  This line is my Access modifier, Return type , Identifier and the () is for the params
->>>>>>> 12e084df107dcb21df78dcf43e6039cac0f03d1f
+
     public static void main(String[] args) {
 
 
