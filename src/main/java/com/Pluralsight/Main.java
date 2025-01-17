e289c74f2ca938555b656b43ac7e521c914e7f97
TumeloM71 <tumelomarongwe@gmail.com>
2025-01-17 12:39:48 -0600

    Update Main.java
    Removed line that retrieved transactions from csv since the database has them

--- src/main/java/com/Pluralsight/Main.java
+++ src/main/java/com/Pluralsight/Main.java
@@ -7,8 +7,7 @@ public class Main {
     //Comment by Tumelo to test commit & push on new branch
     //  This line is my Access modifier, Return type , Identifier and the () is for the params
     public static void main(String[] args) {
-
-        LedgerDAO.getFromCSV();
+       
         ScreenManager screenManager = new ScreenManager();
         screenManager.homeScreen();
     }
